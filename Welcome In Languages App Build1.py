7f9e4fd03f061203ee671fc6fe27bf3d6dd3fbbd
Rishonvr070 <92759829+Rishonvr070@users.noreply.github.com>
2022-09-30 15:31:55 -0400

    Add files via upload

---
+++ Welcome In Languages App Build1.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 26 21:01:05 2022
+
+@author: Admin
+"""
+
+from tkinter import*
+from PIL import Image, ImageTk
+root=Tk()
+root.iconbitmap("favicon1.ico")
+root.title("Welcome In Languages")
+root.geometry("600x600")
+root.configure(bg="Royal Blue")
+
+
+myLabel = Label(root, text="Ways of Saying Welcome in Different Types of Languages",font=('Times New Roman',15,'bold'))
+myLabel.place(relx=0.5, rely=0.05, anchor=CENTER)
+
+
+myLabel = Label(root, text="Welcome",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.08, rely=0.2, anchor=CENTER)
+
+myLabel = Label(root, text="English",font=('Arial',7))
+myLabel.place(relx=0.08, rely=0.3, anchor=CENTER)
+
+myLabel = Label(root, text="Bonjour",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.25, rely=0.2, anchor=CENTER)
+
+myLabel = Label(root, text="Français(French)",font=('Arial',7))
+myLabel.place(relx=0.25, rely=0.3, anchor=CENTER)
+
+myLabel = Label(root, text="स्वागत",font=("Times New Roman",10,'bold'))
+myLabel.place(relx=0.42, rely=0.2, anchor=CENTER)
+
+myLabel = Label(root, text="हिन्दी(Hindi)",font=('Arial',7))
+myLabel.place(relx=0.42, rely=0.3, anchor=CENTER)
+
+myLabel = Label(root, text="வரவேற்பு",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.59, rely=0.2, anchor=CENTER)
+
+myLabel = Label(root, text="தமிழ்(Tamil)",font=('Arial',7))
+myLabel.place(relx=0.59, rely=0.3, anchor=CENTER)
+
+myLabel = Label(root, text="歡迎",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.76, rely=0.2, anchor=CENTER)
+
+myLabel = Label(root, text="中國人(Chinese(Traditional))",font=('Arial',7))
+myLabel.place(relx=0.76, rely=0.3, anchor=CENTER)
+
+myLabel = Label(root, text="いらっしゃいませ",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.93, rely=0.2, anchor=CENTER)
+
+myLabel = Label(root, text="日本(Japanese)",font=('Arial',7))
+myLabel.place(relx=0.93, rely=0.3, anchor=CENTER)
+
+myLabel = Label(root, text="స్వాగతం",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.08, rely=0.4, anchor=CENTER)
+
+myLabel = Label(root, text="తెలుగు(Telugu)",font=('Arial',7))
+myLabel.place(relx=0.08, rely=0.5, anchor=CENTER)
+
+myLabel = Label(root, text="ਸੁਆਗਤ ਹੈ",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.25, rely=0.4, anchor=CENTER)
+
+myLabel = Label(root, text="ਪੰਜਾਬੀ(Punjabi)",font=('Arial',7))
+myLabel.place(relx=0.25, rely=0.5, anchor=CENTER)
+
+myLabel = Label(root, text="Bienvenidos",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.42, rely=0.4, anchor=CENTER)
+
+myLabel = Label(root, text="Español(Spanish)",font=('Arial',7))
+myLabel.place(relx=0.42, rely=0.5, anchor=CENTER)
+
+myLabel = Label(root, text="Bienvenidas(Feminine)",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.59, rely=0.4, anchor=CENTER)
+
+myLabel = Label(root, text="Española(Spanish)",font=('Arial',7))
+myLabel.place(relx=0.59, rely=0.5, anchor=CENTER)
+
+myLabel = Label(root, text="Добро пожаловать",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.76, rely=0.4, anchor=CENTER)
+
+myLabel = Label(root, text="Русский(Russian)",font=('Arial',7))
+myLabel.place(relx=0.76, rely=0.5, anchor=CENTER)
+
+myLabel = Label(root, text="Bem-vindo",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.93, rely=0.4, anchor=CENTER)
+
+myLabel = Label(root, text="Português(Portuguese)",font=('Arial',7))
+myLabel.place(relx=0.93, rely=0.5, anchor=CENTER)
+
+myLabel = Label(root, text="Wolkom",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.08, rely=0.6, anchor=CENTER)
+
+myLabel = Label(root, text="Westerfrysk(Western Frisian)",font=('Arial',7))
+myLabel.place(relx=0.08, rely=0.7, anchor=CENTER)
+
+myLabel = Label(root, text="Benarrivato",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.25, rely=0.6, anchor=CENTER)
+
+myLabel = Label(root, text="Italiano(Italian)",font=('Arial',7))
+myLabel.place(relx=0.25, rely=0.7, anchor=CENTER)
+
+myLabel = Label(root, text="Benarrivata(Feminine)",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.42, rely=0.6, anchor=CENTER)
+
+myLabel = Label(root, text="Italiana(Italian)",font=('Arial',7))
+myLabel.place(relx=0.42, rely=0.7, anchor=CENTER)
+
+myLabel = Label(root, text="Maligayang pagdating",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.59, rely=0.6, anchor=CENTER)
+
+myLabel = Label(root, text="Filipino",font=('Arial',7))
+myLabel.place(relx=0.59, rely=0.7, anchor=CENTER)
+
+myLabel = Label(root, text="Chào mừng",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.76, rely=0.6, anchor=CENTER)
+
+myLabel = Label(root, text="Tiếng Việt(Vietnamese)",font=('Arial',7))
+myLabel.place(relx=0.76, rely=0.7, anchor=CENTER)
+
+myLabel = Label(root, text="Herzlich willkommen",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.93, rely=0.6, anchor=CENTER)
+
+myLabel = Label(root, text="Deutsch(German)",font=('Arial',7))
+myLabel.place(relx=0.93, rely=0.7, anchor=CENTER)
+
+myLabel = Label(root, text="Bine ati venit",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.08, rely=0.8, anchor=CENTER)
+
+myLabel = Label(root, text="Română(Romanian)",font=('Arial',7))
+myLabel.place(relx=0.08, rely=0.9, anchor=CENTER)
+
+myLabel = Label(root, text="خوش آمدی",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.25, rely=0.8, anchor=CENTER)
+
+myLabel = Label(root, text="فارسی(Persian)",font=('Arial',7))
+myLabel.place(relx=0.25, rely=0.9, anchor=CENTER)
+
+myLabel = Label(root, text="أهلا وسهلا",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.42, rely=0.8, anchor=CENTER)
+
+myLabel = Label(root, text="عربي(Arabic)",font=('Arial',7))
+myLabel.place(relx=0.42, rely=0.9, anchor=CENTER)
+
+myLabel = Label(root, text="સ્વાગત છે",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.59, rely=0.8, anchor=CENTER)
+
+myLabel = Label(root, text="ગુજરાતી(Gujarati)",font=('Arial',7))
+myLabel.place(relx=0.59, rely=0.9, anchor=CENTER)
+
+myLabel = Label(root, text="Welina",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.76, rely=0.8, anchor=CENTER)
+
+myLabel = Label(root, text="ʻŌlelo Hawaiʻi(Hawaiian)",font=('Arial',7))
+myLabel.place(relx=0.76, rely=0.9, anchor=CENTER)
+
+myLabel = Label(root, text="Kaabo",font=('Times New Roman',10,'bold'))
+myLabel.place(relx=0.93, rely=0.8, anchor=CENTER)
+
+myLabel = Label(root, text="Yoruba",font=('Arial',7))
+myLabel.place(relx=0.93, rely=0.9, anchor=CENTER)
+
+
+
+
+
+
+
+
+root.mainloop()
